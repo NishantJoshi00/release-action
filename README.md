fcfca653c60337e5af2c5ac357ca52d000cbb579
Nishant Joshi <joshinishant@duck.com>
2022-02-12 01:04:44 +0530

    v0.1 Commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Release Action
+
+Want help creating releases?
+We got you covered.
+
+a simple to implement workflow to add a release easily to your project.
+
+## Usage
+
+```yaml
+# release.yml
+jobs:
+    release:
+        runs-on: ubuntu-latest
+        name: Create Release
+        steps:
+            - name: Checkout
+                uses: actions/checkout@v2
+            - name: Release Action
+                uses: ./
+                id: release-action # Could be anything
+                env:
+                    GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+```
+
+The environment variable `GITHUB_TOKEN` is required to create a release.
+without it, the action will fail.
+
+
+## Configuration
+
+For the tags.yml file or if needed you can change the name by specifying the `config-file` parameter.
+
+```yaml
+# tags.yml
+name: <required> # name of the release
+description: <optional> # description of the release
+description-file: <optional> # path to the description file
+```
+
+## Contributing
+
+Any contributions are welcome. Feel free to open an issue or create a pull request.
---
+++ index.js
@@ -0,0 +1,80 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const yaml = require("js-yaml");
+const fs = require("fs");
+
+const octokit = new github.getOctokit(process.env.GITHUB_TOKEN);
+const owner = process.env.GITHUB_REPOSITORY.split("/")[0];
+const repo = process.env.GITHUB_REPOSITORY.split("/")[1];
+
+async function getTags() {
+    return octokit.rest.repos.getLatestRelease({ owner, repo })
+        .then(res => {
+            return res.data.tag_name;
+        })
+        .catch(err => {
+            return null;
+        })
+}
+
+function loadTagsFile() {
+    const fileName = core.getInput("config-file");
+    const filePath = `${process.env.GITHUB_WORKSPACE}/${fileName}`;
+    const tags = yaml.load(fs.readFileSync(filePath, "utf8"));
+    // Raise an error if the file is empty or no title is provided
+    if (!tags || !tags.name) {
+        throw new Error(`${fileName} is empty or missing a title or version`);
+    }
+    const name = tags.name;
+
+    if (tags.description) {
+        const body = tags.description;
+        return { name, body };
+    } else if (tags.description_file) {
+        const body = fs.readFileSync(`${process.env.GITHUB_WORKSPACE}/${tags.description_file}`, "utf8");
+        return { name, body };
+    } else {
+        return { name, body: "" };
+    }
+}
+
+
+async function add_tag_to_github(latest_tag, new_tag) {
+    if (latest_tag != null) {
+        if (latest_tag == new_tag) {
+            // Exit early if the tag is already on the repo
+            core.notice(`Tag ${new_tag} already exists on the repo`);
+            return;
+        }
+    }
+    core.info(`Trying to add ${new_tag.name}`);
+    return await octokit.rest.repos.createRelease({
+        owner,
+        repo,
+        tag_name: new_tag.name,
+        body: new_tag.body,
+        target_commitish: core.getInput("branch"),
+        generate_release_notes: true,
+    })
+}
+
+
+async function run() {
+    try {
+        const new_tag = loadTagsFile();
+        core.info(`New tag: ${new_tag.name}`);
+        const latest_tag = await getTags();
+        core.info(`Latest tag: ${latest_tag}`);
+        await add_tag_to_github(latest_tag, new_tag);
+    } catch (error) {
+        core.setFailed(`Failed to add tag; reason: ${error}`);
+    }
+}
+
+run()
+    .then(res => {
+        core.info(`Tag added successfully`);
+    })
+    .catch(err => {
+        core.setFailed(err.message);
+    });
